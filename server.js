9ecd832e57bedc194f517c4bf703a1db71438308
somnathsaha2015 <somnathsaha2005@gmail.com>
2016-11-20 00:56:13 +0530

    134

--- server.js
+++ server.js
@@ -1,21 +1,86 @@
-// BASE SETUP
-// ==============================================
-
+"use strict";
+var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+var def = require('./artifacts/definitions');
+var messages = require('./artifacts/messages');
 var express = require('express');
-var app     = express();
-var port    =   process.env.PORT || 8080;
+var app = express();
 
-// ROUTES
-// ==============================================
+app.set('messages', messages);
+app.set('def', def);
+app.set('config', config);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// sample route with a route the way we're used to seeing it
-app.get('/', function(req, res) {
-    res.send('this is a sample!');  
+var router = require('./artifacts/router');
+app.use(router);
+router.init(app);
+
+process.env.NODE_ENV = config.env;
+
+//for cross domain
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, Authorization, Content-Length, X-Requested-With, Access-Control-Allow-Origin,x-access-token');
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+app.use(allowCrossDomain);
+
+app.get('/*', function (req, res) {console.log('index.html');
+    res.sendFile(path.join(__dirname, 'public','index.html'));
+    //res.render(path.join(__dirname, 'public/index1.html'));
 });
 
-// we'll create our routes here
+//app.get('/', function (req, res, next) {
+    //res.send(messages.genInfo);
+    //res.sendFile(path.join(__dirname, 'public','index1.html'));
+//});
+var port    =   process.env.PORT || 8080;
+var server = app.listen(port, function () {
+    console.log(messages.messExpressServerRunning + config.port);
+});
 
-// START THE SERVER
-// ==============================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+app.use(function (err, req, res, next) {
+    if (err) {
+        if (err.hasOwnProperty('body')) {
+            var error = new def.NError(err.status, err.message, err.body);
+            err = error;
+        }
+    }
+    else {
+        err = new def.NError(404, messages.errUrlNotFound, messages.messUrlNotFoundDetails);
+    }
+    next(err);
+});
+//development error handler
+if (process.env.NODE_ENV === 'development') {
+    app.use(function (err, req, res, next) {
+        console.log(messages.errDevError);
+        if (!res.finished) {
+            res.status(err.status || 500);
+            res.json({ error: err });
+        }
+    });
+}
+//production error handler
+app.use(function (err, req, res, next) {
+    console.log(messages.errProdError);
+    //if (err.message) { delete err.message; }
+    //if (err.details) { delete err.details; }
+    if (!res.finished) {
+        res.status(err.status || 500);
+        res.json({ error: err });
+    }
+});
