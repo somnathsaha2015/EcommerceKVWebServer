8ee236303b9e8ec827cc53e4ff40a32a040673f5
somnathsaha2015 <somnathsaha2005@gmail.com>
2016-11-19 19:11:54 +0530

    116

--- public/app/config.js
+++ public/app/config.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.urlHash = {
-    'post:authenticate': '/api/authenticate',
+    'post:authenticate': '/authenticate',
     'post:validate:token': '/api/validate/token',
     'post:forgot:password': '/api/forgot/password',
     'post:send:password': '/api/send/password',
